e8b0a0bf048c91d88d886afb71f86b1d2eba76f6
Jade1724 <haruka.i1724@gmail.com>
2025-01-25 11:39:08 +1300

    feat: thought bubble periodically showing and dismiss on speech bubble showing

--- Character/base_character.gd
+++ Character/base_character.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var speech_bubble = $SpeechBubble
+@onready var thought_bubble = $ThoughtBubble
+@onready var thought_bubble_timer = $ThoughtBubbleTimer
 @onready var click_area = $ClickArea
 
 # Called when the node enters the scene tree for the first time.
@@ -10,9 +12,15 @@ func _ready() -> void:
 
 func _on_click_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if !speech_bubble.visible and event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		print("make it visible")
+		thought_bubble.visible = false
+		thought_bubble_timer.stop()
 		speech_bubble.visible = true
 
 
 func _on_click_area_mouse_exited() -> void:
 	speech_bubble.visible = false
+	thought_bubble_timer.start()
+
+
+func _on_thought_bubble_timer_timeout() -> void:
+	thought_bubble.visible =  !thought_bubble.visible
