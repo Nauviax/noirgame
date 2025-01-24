913129bed08ffaa9bbb818bd430855d8849eced9
Nauviax <Nauviax@icloud.com>
2025-01-25 10:23:52 +1300

    Basic location system (wip ofc)

---
+++ scenes/locations/base_location.gd
@@ -0,0 +1,5 @@
+extends Node2D
+class_name Location
+
+## Name of location, unique
+@export var location_name: String = "Base Location"
--- scenes/main_scene.gd
+++ scenes/main_scene.gd
@@ -1,11 +1,21 @@
+@tool # Run in editor.
 extends Node2D
+class_name MainScene
 
+## Currently loaded location.
+var current_location: Location
 
-# Called when the node enters the scene tree for the first time.
+## TEMP var holding a location to load.
+@export var initial_location: PackedScene
+
+## Onready, load the initial location.
 func _ready():
-	pass # Replace with function body.
+	load_location(initial_location)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+## Load a new location, unloading the current one.
+func load_location(new_location: PackedScene):
+	if current_location != null:
+		current_location.queue_free()
+	current_location = new_location.instantiate()
+	add_child(current_location)
