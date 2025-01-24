1c54bf505b57b267421261a2be5d1e3db6232cdb
Nauviax <Nauviax@icloud.com>
2025-01-25 11:37:31 +1300

    Toolbar work, added placeholder textures

--- scenes/locations/base_location.gd
+++ scenes/locations/base_location.gd
@@ -3,3 +3,6 @@ class_name Location
 
 ## Name of location, unique
 @export var location_name: String = "Base Location"
+
+## Image to use for selector button
+@export var location_icon: Texture
--- scenes/main_scene.gd
+++ scenes/main_scene.gd
@@ -2,38 +2,36 @@
 extends Node2D
 class_name MainScene
 
-
-@export_category("PackedScenes")
-
 ## Array of all locations, packed scenes.
-@export var locations: Array[PackedScene]
+@export var locations_unloaded: Array[PackedScene]
+
+## Array of instantiated locations.
+var locations: Array[Location] = []
 
 ## Reference to location button scene, to populate UI with options.
 @export var location_button: PackedScene
 
-
-@export_category("UI elements")
-
 ## Location selector UI element, to add children to.
 @export var location_selector: Control
 # (I like adding references to UI like this, so if we move it around later it doesn't break the scripts.)
-
 
 ## Currently loaded location.
 var current_location: Location
 
 ## Onready, load the initial location and populate the UI.
 func _ready():
-	load_location(locations[0])
+	for location in locations_unloaded:
+		locations.push_back(location.instantiate())
+	show_location(locations[0])
 	for location in locations:
 		var button: LocationButton = location_button.instantiate()
 		button.location_to_load = location
+		button.texture_normal = location.location_icon
 		location_selector.add_child(button)
 
-
 ## Load a new location, unloading the current one.
-func load_location(new_location: PackedScene):
-	if current_location != null: # Shouldn't ever be false
-		current_location.queue_free()
-	current_location = new_location.instantiate()
+func show_location(new_location: Location):
+	if current_location: # Shouldn't ever be false.
+		remove_child(current_location)
+	current_location = new_location
 	add_child(current_location)
