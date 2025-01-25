2c04a84e8028a905515423962b49bf4565dabad0
Jade1724 <haruka.i1724@gmail.com>
2025-01-25 19:16:56 +1300

    feat: randomised thought bubble popup time and bubble positions

--- character/base_character.gd
+++ character/base_character.gd
@@ -5,9 +5,19 @@ extends Node2D
 @onready var thought_bubble_timer = $ThoughtBubbleTimer
 @onready var click_area = $ClickArea
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+	
+func _ready():
+	# Randomize a delay between 0 and 5 seconds
+	var random_delay = randf_range(0, 5)
+	
+	# Print the randomized delay
+	print("Timer will start after delay: ", random_delay)
+	
+	# Delay the start of the timer by the randomized amount
+	await get_tree().create_timer(random_delay).timeout
+	
+	# Start the Timer node
+	thought_bubble_timer.start()
 	
 
 func _on_click_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
