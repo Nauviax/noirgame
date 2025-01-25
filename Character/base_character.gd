81ed61dc3837f1223505217605ca1edf3961ceaf
Jade1724 <haruka.i1724@gmail.com>
2025-01-25 14:12:22 +1300

    feat: replaced character images and updated character1

--- Character/base_character.gd
+++ Character/base_character.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var speech_bubble = $SpeechBubble
 @onready var thought_bubble = $ThoughtBubble
+@onready var name_panel = $NamePanel
 @onready var thought_bubble_timer = $ThoughtBubbleTimer
 @onready var click_area = $ClickArea
 
@@ -21,7 +22,13 @@ func _on_click_area_mouse_exited() -> void:
 	if (speech_bubble.visible):
 		speech_bubble.visible = false
 		thought_bubble_timer.start()
+		
+	name_panel.visible = false
 
 
 func _on_thought_bubble_timer_timeout() -> void:
 	thought_bubble.visible =  !thought_bubble.visible
+
+
+func _on_click_area_mouse_entered() -> void:
+	name_panel.visible = true
