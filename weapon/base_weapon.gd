d7f0143ac95bdc5e59910700043ae5c1376e2109
Jade1724 <haruka.i1724@gmail.com>
2025-01-26 13:59:03 +1300

    feat: base weapon scene created

---
+++ weapon/base_weapon.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var thought_bubble = $ThoughtBubble
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	thought_bubble.visible = false
+
+
+func _on_click_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if !thought_bubble.visible and event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		thought_bubble.visible = true
+		$ClickSound.play()
+
+
+func _on_click_area_mouse_exited() -> void:
+	thought_bubble.visible = false
